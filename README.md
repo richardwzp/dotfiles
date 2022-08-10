bc468e58feffe4c57ffe842693525c8ce0c49445
richardwzp <wang.zipeng@northeastern.edu>
2022-08-10 09:47:12 -0400

    improved readme, added some more install for the install script

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 Just in case I need to use them somewhere
+
+**How to initialize:**
+first, create a `/.dotfiles` directory, then initialize the barebone repo
+```bash
+git init --bare $HOME/.dotfiles
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+config config --local status.showUntrackedFiles no
+echo "alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.bashrc
+```
+
+**How to use:**
+```bash
+git clone --bare git@github.com:richardwzp/dotfiles.git $HOME/.dotfiles
+```
+the `dot` command inside of zshrc is used to create a macro for managing this barebone repo. source the `.zshrc` file before trying to do more git operations.
--- dot_install.sh
+++ dot_install.sh
@@ -1,8 +1,23 @@
 #!/bin/zsh
 
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+
+# install neovim
+brew install neovim
+# install vimplug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# install packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+# install utilities
 brew install ripgrep
 brew install fd
 
