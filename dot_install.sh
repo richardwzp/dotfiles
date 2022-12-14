6c09213d5815d408da682465c1a803b77218c496
richardwzp <wang.zipeng@northeastern.edu>
2022-12-14 13:38:18 -0500

    add font install and more automation to install.sh

--- dot_install.sh
+++ dot_install.sh
@@ -19,6 +19,9 @@ source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 # power 10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+# install font
+brew tap homebrew/cask-fonts
+brew install --cask font-hack-nerd-font
 # install neovim
 brew install neovim
 # install better git
@@ -30,7 +33,8 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # install rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sh <(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs) -y
 rustc --version
 
 # install packer
@@ -54,7 +58,7 @@ brew install tmux
 # git clone git@github.com:richardwzp/tmux-sensible.git
 
 # need to install make
-LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) --yes
 
 
 
